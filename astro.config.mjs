193d58805af2e517d035827a21b96fc1840a5e10
Bharat Mhaskar <bmhaskar@gmail.com>
2022-08-28 11:32:46 -0700

    Migrated the first blog along with image support and emoji support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,23 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+import emoji from "remark-emoji";
+import a11yEmoji from "@fec/remark-a11y-emoji";
+import rehypeStringify from "rehype-stringify";
+import remarkRehype from "remark-rehype";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://bmhaskar.com",
-  integrations: [mdx(), sitemap()],
-  noExternal: ["open-props"],
+  markdown: {
+    drafts: true
+  },
+  integrations: [mdx({
+    remarkPlugins: {
+      extends: [emoji, a11yEmoji, remarkRehype, rehypeStringify]
+    }
+  }), sitemap(), image()],
+  noExternal: ["open-props"]
 });
